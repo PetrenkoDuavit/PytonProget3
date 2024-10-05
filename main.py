07ea9fe0addc53116cd6490780c8676554068a72
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-10-05 07:35:38 +0300

    HW3.3

--- main.py
+++ main.py
@@ -16,24 +16,39 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
-# Homeworck 3.1
-a = int(input("Enter First number: ")) #Ввод числел для вычислений
-b = int(input("Enter Second number: "))
+# # Homeworck 3.1
+# a = int(input("Enter First number: ")) #Ввод числел для вычислений
+# b = int(input("Enter Second number: "))
+#
+# user_select = input("Chouse from - + / *. and press enter: ") # выбор действия
+#
+# match user_select: #
+#     case ("-"): #
+#         print( a - b)# вычисление и вывод в консоль результата
+#     case ("+"):
+#         print(a + b)
+#     case ("*"):
+#         print(a * b)
+#     case ("/"):
+#         if b == 0:
+#             b = int(input("Invalid number, select enother number: ")) # перевірка ділення на 0, з мозжливістю виправитись
+#         print( a / b)
+#     case _:
+#         print("Invalid input please try again")
+# Homeworck 3.2
 
-user_select = input("Chouse from - + / *. and press enter: ") # выбор действия
-
-match user_select: #
-    case ("-"): #
-        print( a - b)# вычисление и вывод в консоль результата
-    case ("+"):
-        print(a + b)
-    case ("*"):
-        print(a * b)
-    case ("/"):
-        if b == 0:
-            b = int(input("Invalid number, select enother number: ")) # перевірка ділення на 0, з мозжливістю виправитись
-        print( a / b)
-    case _:
-        print("Invalid input please try again")
 
 
+# Homeworck 3.3
+
+numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9] # В задании не было указано что нужно сделать инпут, поэтому сделал так..
+if len(numbers) % 2:  # Проверка длины массива, четная или нечетная
+ lengthOfArr1 =  len(numbers) // 2 + 1 # сдвигаю на 1 для того чтобы правильно записать длину в новый массив
+ lengthOfArr2 = len(numbers) // 2 + 1
+else:                 # во всех других случаях делю на 2
+    lengthOfArr1 = len(numbers) // 2
+    lengthOfArr2 = len(numbers)// 2
+part1 = numbers[:lengthOfArr1] # создаю новые массивы для вывода в консоль
+part2 = numbers[lengthOfArr2:]
+print(part1)
+print(part2)

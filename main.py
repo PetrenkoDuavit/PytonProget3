f3bdfe01bdd1dba9a6b9cb5452e61e9737f3c704
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-10-06 07:57:17 +0300

    HW3.2(0)

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
 # # Homeworck 3.1
@@ -35,20 +20,28 @@ if __name__ == '__main__':
 #         print( a / b)
 #     case _:
 #         print("Invalid input please try again")
+
 # Homeworck 3.2
 
-
+numbersList = [12, 5, 5, 7, 4, 9]
+lengOflist = len(numbersList) # помещаю длину списка в переменную lengOflist для проверок If else
+if 0 == lengOflist:  # если список пустой сразу вывожу в консоль
+    print(numbersList)
+else:
+    lastNumerOfList = numbersList.pop() # удаляю последний элемент списка и помещаю в переменную
+    numbersList.insert(0, lastNumerOfList) # записываю удаленный элемент в начало списка
+    print(numbersList)
 
 # Homeworck 3.3
 
-numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9] # В задании не было указано что нужно сделать инпут, поэтому сделал так..
-if len(numbers) % 2:  # Проверка длины массива, четная или нечетная
- lengthOfArr1 =  len(numbers) // 2 + 1 # сдвигаю на 1 для того чтобы правильно записать длину в новый массив
- lengthOfArr2 = len(numbers) // 2 + 1
-else:                 # во всех других случаях делю массив на 2
-    lengthOfArr1 = len(numbers) // 2
-    lengthOfArr2 = len(numbers)// 2
-part1 = numbers[:lengthOfArr1] # создаю новые массивы для вывода в консоль
-part2 = numbers[lengthOfArr2:]
-print(part1)
-print(part2)
+# numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9] # В задании не было указано что нужно сделать инпут, поэтому сделал так..
+# if len(numbers) % 2:  # Проверка длины массива, четная или нечетная
+#  lengthOfArr1 =  len(numbers) // 2 + 1 # сдвигаю на 1 для того чтобы правильно записать длину в новый массив
+#  lengthOfArr2 = len(numbers) // 2 + 1
+# else:                 # во всех других случаях делю массив на 2
+#     lengthOfArr1 = len(numbers) // 2
+#     lengthOfArr2 = len(numbers)// 2
+# part1 = numbers[:lengthOfArr1] # создаю новые массивы для вывода в консоль
+# part2 = numbers[lengthOfArr2:]
+# print(part1)
+# print(part2)
